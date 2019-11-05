80c0d259eeaffb422b006f9814addae4b7e4b173
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2019-11-06 00:05:40 +0530

    Redisigned registration page

--- lib/pages/registration.dart
+++ lib/pages/registration.dart
@@ -20,10 +20,6 @@ class _RegistrationState extends State<Registration> {
   String txnRef = '';
   String approvalRef = '';
   String status = '';
-  ValueNotifier<int> _index = ValueNotifier(0);
-  StepState step0 = StepState.indexed,
-      step1 = StepState.indexed,
-      step2 = StepState.indexed;
 
   Future<String> initTransaction(String app) async {
     UpiIndia upi = new UpiIndia(
@@ -41,51 +37,42 @@ class _RegistrationState extends State<Registration> {
   }
 
   Future<bool> registerUser() {
-    return Firestore.instance.collection('registered').add({
-      'name': widget.user['name'],
-      'photoUrl': widget.user['photoUrl'],
-      'email': widget.user['email'],
-      'txnId': txnId,
-      'status': status,
-      'approvalRefNumber': approvalRef,
-    }).then((value) {
+    return Firestore.instance
+        .collection('registered')
+        .where('email', isEqualTo: widget.user['email'])
+        .getDocuments()
+        .then((QuerySnapshot snapshot) {
+      print(snapshot.documents.length);
+      if (snapshot.documents.length == 0) {
+        return Firestore.instance.collection('registered').add({
+          'name': widget.user['name'],
+          'photoUrl': widget.user['photoUrl'],
+          'email': widget.user['email'],
+          'txnId': txnId,
+          'status': status,
+          'approvalRefNumber': approvalRef,
+        }).then((value) {
+          return true;
+        }).catchError((error) {
+          return false;
+        });
+      }
       return true;
-    }).catchError((error) {
-      return false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     Widget _instructionBody = Container(
-      child: Column(
-        children: <Widget>[
-          Text(
-            'This is the registration for Anweshan 2020. After completing the payment you ' +
-                'will get your transaction details. Please be sure to keep a screenshot of the ' +
-                'details for future queries. Please stay online during the transaction and do not ' +
-                'press the back button. For any query contact our support team.',
-            textAlign: TextAlign.justify,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              FlatButton(
-                child: Text('Next'),
-                textColor: Colors.green,
-                onPressed: () {
-                  step0 = StepState.complete;
-                  _index.value = 1;
-                },
-              ),
-              FlatButton(
-                child: Text('Cancel'),
-                textColor: Colors.red,
-                onPressed: () => Navigator.of(context).pop(),
-              )
-            ],
-          ),
-        ],
+      child: Text(
+        'This is the registration for Anweshan 2020. After completing the payment you ' +
+            'will get your transaction details. Be sure to keep a copy of the ' +
+            'details for future queries. Please stay online during the transaction and do not ' +
+            'press the back button. For any query contact our support team.',
+        textAlign: TextAlign.justify,
+        style: TextStyle(
+          fontSize: 16,
+        ),
       ),
     );
 
@@ -93,6 +80,7 @@ class _RegistrationState extends State<Registration> {
         child: Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
       children: <Widget>[
         Text(
           'Pay using :',
@@ -104,7 +92,7 @@ class _RegistrationState extends State<Registration> {
         Wrap(
           direction: Axis.horizontal,
           spacing: 0.0,
-          runSpacing: 32.0,
+          runSpacing: 30.0,
           children: <Widget>[
             FlatButton(
               child: ClipOval(
@@ -164,194 +152,119 @@ class _RegistrationState extends State<Registration> {
             ),
           ],
         ),
-        SizedBox(height: 25.0),
-        FutureBuilder(
-          future: _initiateTransaction,
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            TextStyle _textStyle =
-                TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500);
-            if (snapshot.connectionState == ConnectionState.waiting ||
-                snapshot.data == null)
-              return Text(' ');
-            else {
-              print(snapshot.data.toString());
-              switch (snapshot.data.toString()) {
-                case UpiIndiaResponseError.APP_NOT_INSTALLED:
-                  return Text(
-                    'App not installed.',
-                    style: _textStyle,
-                  );
-                  break;
-                case UpiIndiaResponseError.INVALID_PARAMETERS:
-                  return Text(
-                    'Requested app rejected to proceed further.',
-                    style: _textStyle,
-                  );
-                  break;
-                case UpiIndiaResponseError.USER_CANCELLED:
-                  return Text(
-                    'It seems like you cancelled the transaction.',
-                    style: _textStyle,
-                  );
-                  break;
-                case UpiIndiaResponseError.NULL_RESPONSE:
-                  return Text(
-                    'No data received',
-                    style: _textStyle,
-                  );
-                  break;
-                default:
-                  UpiIndiaResponse _flutterUpiResponse;
-                  _flutterUpiResponse = UpiIndiaResponse(snapshot.data);
-                  txnId = _flutterUpiResponse.transactionId;
-                  txnRef = _flutterUpiResponse.transactionRefId;
-                  status = _flutterUpiResponse.status;
-                  approvalRef = _flutterUpiResponse.approvalRefNo;
-                  print(status);
-                  if (status == UpiIndiaResponseStatus.SUCCESS) {
-                    return FutureBuilder(
-                      future: registerUser(),
-                      builder:
-                          (BuildContext context, AsyncSnapshot<bool> snapshot) {
-                        if (snapshot.connectionState == ConnectionState.done) {
-                          if (snapshot.hasError)
-                            return Text(
-                              'Error occured during registration. Contact Support.',
-                              style: _textStyle,
-                            );
-                          else if (snapshot.data) {
-                            
-                            return Column(
-                              children: <Widget>[
-                                Text(
-                                  'Registered Succesfully!',
-                                  style: _textStyle,
-                                ),
-                                FlatButton(
-                                  child: Text('Transaction Summary'),
-                                  textColor: Colors.green,
-                                  onPressed: () {
-                                    step1 = StepState.complete;
-                                    step2 = StepState.complete;
-                                    _index.value = 2;
-                                    setState(() {});
-                                  },
-                                ),
-                              ],
-                            );
-                          } else {
-                            return Text(
-                              'Failed to register. Contact Support.',
-                              style: _textStyle,
-                            );
-                          }
-                        }
-                        return CircularProgressIndicator();
-                      },
-                    );
-                  } else if (status == UpiIndiaResponseStatus.SUBMITTED) {
+        SizedBox(
+          height: 30,
+        ),
+        Center(
+          child: FutureBuilder(
+            future: _initiateTransaction,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              TextStyle _textStyle =
+                  TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500);
+              if (snapshot.connectionState == ConnectionState.waiting ||
+                  snapshot.data == null)
+                return Text(' ');
+              else {
+                print(snapshot.data.toString());
+                switch (snapshot.data.toString()) {
+                  case UpiIndiaResponseError.APP_NOT_INSTALLED:
                     return Text(
-                      'Looks like your transaction didn\'t complete. Cancel it or contact support',
+                      'App not installed.',
                       style: _textStyle,
                     );
-                  } else {
-                    return Text('Payment Failed', style: _textStyle,);
-                  }
+                    break;
+                  case UpiIndiaResponseError.INVALID_PARAMETERS:
+                    return Text(
+                      'Requested app rejected to proceed further.',
+                      style: _textStyle,
+                    );
+                    break;
+                  case UpiIndiaResponseError.USER_CANCELLED:
+                    return Text(
+                      'It seems like you cancelled the transaction.',
+                      style: _textStyle,
+                    );
+                    break;
+                  case UpiIndiaResponseError.NULL_RESPONSE:
+                    return Text(
+                      'No data received',
+                      style: _textStyle,
+                    );
+                    break;
+                  default:
+                    UpiIndiaResponse _flutterUpiResponse;
+                    _flutterUpiResponse = UpiIndiaResponse(snapshot.data);
+                    txnId = _flutterUpiResponse.transactionId;
+                    txnRef = _flutterUpiResponse.transactionRefId;
+                    status = _flutterUpiResponse.status;
+                    approvalRef = _flutterUpiResponse.approvalRefNo;
+                    print(status);
+                    if (status == UpiIndiaResponseStatus.SUCCESS) {
+                      return FutureBuilder(
+                        future: registerUser(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<bool> snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.done) {
+                            if (snapshot.hasError)
+                              return Text(
+                                'Error occured during registration. Contact Support.',
+                                style: _textStyle,
+                              );
+                            else if (snapshot.data) {
+                              return Column(
+                                children: <Widget>[
+                                  Text(
+                                    'Registered Succesfully!',
+                                    style: _textStyle,
+                                  ),
+                                  FlatButton(
+                                    child: Text('See Transaction Summary'),
+                                    textColor: Colors.green,
+                                    onPressed: () {
+                                      Navigator.of(context).pushReplacement(
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              _TransactionSummary(
+                                            txnId: txnId,
+                                            txnRef: txnRef,
+                                            approvalRef: approvalRef,
+                                            status: status,
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ],
+                              );
+                            } else {
+                              return Text(
+                                'Failed to register. Contact Support.',
+                                style: _textStyle,
+                              );
+                            }
+                          }
+                          return CircularProgressIndicator();
+                        },
+                      );
+                    } else if (status == UpiIndiaResponseStatus.SUBMITTED) {
+                      return Text(
+                        'Looks like your transaction didn\'t complete. Cancel it or contact support',
+                        style: _textStyle,
+                      );
+                    } else {
+                      return Text(
+                        'Payment Failed',
+                        style: _textStyle,
+                      );
+                    }
+                }
               }
-            }
-          },
+            },
+          ),
         ),
       ],
     ));
-
-    TextStyle _summaryHead =
-            TextStyle(fontWeight: FontWeight.w600, fontSize: 20.0),
-        _summaryText = TextStyle(fontSize: 16.0);
-
-    Widget _paymentSummaryBody = Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Text(
-          'Transaction ID:',
-          style: _summaryHead,
-        ),
-        SizedBox(
-          height: 5.0,
-        ),
-        SelectableText(
-          txnId ?? " ",
-          style: _summaryText,
-        ),
-        SizedBox(
-          height: 20.0,
-        ),
-        Text(
-          'Transaction Reference ID:',
-          style: _summaryHead,
-        ),
-        SizedBox(
-          height: 5.0,
-        ),
-        SelectableText(
-          txnRef ?? " ",
-          style: _summaryText,
-        ),
-        SizedBox(
-          height: 20.0,
-        ),
-        Text(
-          'Transaction Status:',
-          style: _summaryHead,
-        ),
-        SizedBox(
-          height: 5.0,
-        ),
-        Text(
-          status.toUpperCase() ?? " ",
-          style: _summaryText,
-        ),
-        SizedBox(
-          height: 20.0,
-        ),
-      ],
-    );
-
-    Widget _body = Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: ValueListenableBuilder(
-        valueListenable: _index,
-        builder: (BuildContext context, int index, Widget child) {
-          return Stepper(
-            steps: [
-              Step(
-                title: Text('Instruction'),
-                content: _instructionBody,
-                state: step0,
-                isActive: true,
-              ),
-              Step(
-                title: Text('Payment'),
-                content: _paymentBody,
-                state: step1,
-                isActive: true,
-              ),
-              Step(
-                title: Text('Payment Summary'),
-                state: step2,
-                isActive: true,
-                content: _paymentSummaryBody,
-              ),
-            ],
-            currentStep: index,
-            controlsBuilder: (BuildContext context,
-                    {VoidCallback onStepContinue, VoidCallback onStepCancel}) =>
-                Container(),
-          );
-        },
-      ),
-    );
 
     return Scaffold(
       backgroundColor: Colors.lightBlue[200],
@@ -380,7 +293,109 @@ class _RegistrationState extends State<Registration> {
             ],
           ),
         ),
-        child: _body,
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Instructions:',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            _instructionBody,
+            SizedBox(
+              height: 20,
+            ),
+            _paymentBody,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _TransactionSummary extends StatelessWidget {
+  final String txnId, txnRef, approvalRef, status;
+
+  _TransactionSummary({
+    this.approvalRef,
+    this.status,
+    this.txnId,
+    this.txnRef,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle _summaryHead =
+            TextStyle(fontWeight: FontWeight.w600, fontSize: 20.0),
+        _summaryText = TextStyle(fontSize: 16.0);
+
+    return Material(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Image.asset(
+                'images/sole_logo.png',
+                height: 150,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Transaction ID:',
+              style: _summaryHead,
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            SelectableText(
+              txnId ?? " ",
+              style: _summaryText,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Transaction Reference ID:',
+              style: _summaryHead,
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            SelectableText(
+              txnRef ?? " ",
+              style: _summaryText,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Transaction Status:',
+              style: _summaryHead,
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text(
+              status.toUpperCase() ?? " ",
+              style: _summaryText,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+          ],
+        ),
       ),
     );
   }
