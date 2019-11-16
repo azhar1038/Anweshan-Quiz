29ec64353a10c343eb9666a73560b02b1a841f6f
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2019-11-16 20:35:16 +0530

    Asks for permission

--- android/app/src/main/java/com/az/quiz/MainActivity.java
+++ android/app/src/main/java/com/az/quiz/MainActivity.java
@@ -3,6 +3,10 @@ package com.az.quiz;
 import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
 import java.util.Map;
@@ -17,6 +21,8 @@ import io.flutter.plugin.common.MethodChannel.Result;
 public class MainActivity extends FlutterActivity {
 
   private static final String CHANNEL = "com.az.quiz/intent";
+
+  private static final int GET_PHONE_PERMISSION_REQUEST_ID = 2546;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +41,15 @@ public class MainActivity extends FlutterActivity {
           }
           
         }else if(methodCall.method.equals("callIntent")){
-          
-          Uri uri = Uri.parse("tel:9439717907");
-          Intent intent = new Intent(Intent.ACTION_CALL, uri);
-          if(intent.resolveActivity(getPackageManager())!=null){
-            startActivity(Intent.createChooser(intent, "Call using..."));
+          if(ContextCompat.checkSelfPermission(MainActivity.this,
+            Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+              ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, GET_PHONE_PERMISSION_REQUEST_ID);
+          }else {
+            Uri uri = Uri.parse("tel:9439717907");
+            Intent intent = new Intent(Intent.ACTION_CALL, uri);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+              startActivity(Intent.createChooser(intent, "Call using..."));
+            }
           }
         }else if(methodCall.method.equals("facebookIntent")){
           Uri uri = Uri.parse("https://www.facebook.com/soleigit/");
@@ -50,5 +60,19 @@ public class MainActivity extends FlutterActivity {
         }
       }
     });
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    if(requestCode == GET_PHONE_PERMISSION_REQUEST_ID){
+      if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        Uri uri = Uri.parse("tel:9439717907");
+        Intent intent = new Intent(Intent.ACTION_CALL, uri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+          startActivity(Intent.createChooser(intent, "Call using..."));
+        }
+      }
+    }
+    //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
   }
 }
